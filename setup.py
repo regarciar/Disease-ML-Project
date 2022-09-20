de6a86b327f97f59bb6d3447388db889ecab5863
regarciar <robert.erick.garcia.rey@gmail.com>
2022-09-20 18:02:34 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='crear un sistema con aprendizaje de maquina que permitadeterminar  automaticamente si un pacciente tiene problemas de corazon basadoen los datos de sus examenes.',
+    author='Uniquindio',
+    license='MIT',
+)
